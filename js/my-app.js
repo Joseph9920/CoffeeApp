7f0eb9f1061e080a7a6e259b15d70a13ad20b78d
Joseph9920 <70551467+Joseph9920@users.noreply.github.com>
2020-12-09 19:09:20 -0600

    CoffeeApp diseño web 2
    Proyecto de creación aplicación.

---
+++ js/my-app.js
@@ -0,0 +1,38 @@
+// Initialize your app
+var myApp = new Framework7({
+	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: false
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'Coffee Shop',
+	dialog: {
+		title: 'Coffee Shop',
+		buttonOk: 'Aceptar',
+  	},
+	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/productos/',
+    	url: 'productos.html',
+    	name: 'productos',
+  		}
+	]
+});
+
+// Export selectors engine
+var $$ = Dom7;
+
+
+
+
+
+ 
+
